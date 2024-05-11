d5d9929a03c208754f199715f1b7d0385822d675
Robson Cruz <ghost0092@gmail.com>
2024-05-10 21:26:49 -0300

    test(main_test.go): update test suite
    With the new function signature, the test suite needed an update so the
    tests go back to a green state. Also added a small test section related
    to the ignore list.

--- main_test.go
+++ main_test.go
@@ -1,27 +1,30 @@
 package main
 
 import (
+	"reflect"
 	"testing"
 )
 
 func TestLeetSpeak(t *testing.T) {
 	tests := []struct {
-		name     string
-		input    string
-		expected string
-		replPerc float64
-		randSeed int64
+		name       string
+		input      string
+		ignoreList string
+		expected   string
+		replPerc   float64
+		randSeed   int64
 	}{
-		{name: "Basic input", input: "hello world", expected: "h3ll0 w0Rld", replPerc: 1.0, randSeed: 1},
-		{name: "Partial replacement", input: "hello world", expected: "heLL0 W0RlD", replPerc: 0.5, randSeed: 1},
-		{name: "When replacement percentage is zero no swaps are made from letter to number", input: "hello world", expected: "heLLO wOrLD", replPerc: 0.0, randSeed: 1},
-		{name: "When passing string with special characters they are kept", input: "-helLo w0rld!", expected: "-h3lL0 w0rLD!", replPerc: 1.0, randSeed: 1},
-		{name: "When passing an empty string an empty string is returned", input: "", expected: "", replPerc: 1.0, randSeed: 1},
+		{name: "Basic input", input: "hello world", ignoreList: "", expected: "h3ll0 w0Rld", replPerc: 1.0, randSeed: 1},
+		{name: "Partial replacement", input: "hello world", ignoreList: "", expected: "heLL0 W0RlD", replPerc: 0.5, randSeed: 1},
+		{name: "When replacement percentage is zero no swaps are made from letter to number", input: "hello world", ignoreList: "", expected: "heLLO wOrLD", replPerc: 0.0, randSeed: 1},
+		{name: "When passing string with special characters they are kept", input: "-helLo w0rld!", ignoreList: "", expected: "-h3lL0 w0rLD!", replPerc: 1.0, randSeed: 1},
+		{name: "When passing an empty string an empty string is returned", input: "", ignoreList: "", expected: "", replPerc: 1.0, randSeed: 1},
+		{name: "When passing an string with an ignore list ignored characters are returned as is", input: "hello", ignoreList: "h,e,l,o", expected: "hello", replPerc: 1.0, randSeed: 1},
 	}
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := leetSpeak(tt.input, tt.replPerc, tt.randSeed); got != tt.expected {
+			if got := leetSpeak(tt.input, tt.ignoreList, tt.replPerc, tt.randSeed); got != tt.expected {
 				t.Errorf("Expected [%v] for leetSpeak(\"%v\", %v, %v), got %v instead.", tt.expected, tt.input, tt.replPerc, tt.randSeed, got)
 			}
 		})
@@ -49,15 +52,44 @@ func TestUnleetSpeak(t *testing.T) {
 	}
 }
 
+func TestCreateIgnoreList(t *testing.T) {
+	t.Run("When receiving properly formatted comma separated input results in a slice of strings", func(t *testing.T) {
+		tests := []struct {
+			input    string
+			expected []string
+		}{
+			{"a,b,c", []string{"a", "b", "c"}},
+			{"1,2,3", []string{"1", "2", "3"}},
+			{"x,y,z", []string{"x", "y", "z"}},
+		}
+
+		for _, test := range tests {
+			result := createIgnoreList(test.input)
+			if !reflect.DeepEqual(result, test.expected) {
+				t.Errorf("Expected %v, but got %v", test.expected, result)
+			}
+		}
+	})
+}
+
 // Benchmark tests
 func BenchmarkLeetSpeak(b *testing.B) {
-	for i := 0; i < b.N; i++ {
-		leetSpeak("hello world", 1.0, 1)
-	}
+	b.Run("Simple case without ignores", func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			leetSpeak("hello world", "", 1.0, 1)
+		}
+	})
+	b.Run("Simple case with ignores", func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			leetSpeak("hello world", "h,e,l,o,w,r,d", 1.0, 1)
+		}
+	})
 }
 
 func BenchmarkUnleetSpeak(b *testing.B) {
-	for i := 0; i < b.N; i++ {
-		unleetSpeak("h3ll0 w0rld")
-	}
+	b.Run("Simple case", func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			unleetSpeak("h3ll0 w0rld")
+		}
+	})
 }
